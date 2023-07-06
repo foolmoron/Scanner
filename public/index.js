109af46f958cee1beddb1af5a5769e3e9ec87290
Momin Khan <foolmoron@gmail.com>
2023-07-05 21:05:24 -0400

    prompt in barcode

--- public/index.js
+++ public/index.js
@@ -1,9 +1,7 @@
 async function onSubmit(event) {
     event.preventDefault();
 
-    const name = event.target.name.value;
-    const doc = await db.collection('tickets').add({
-        name,
-    });
+    const data = Object.fromEntries(new FormData(event.target).entries());
+    const doc = await db.collection('tickets').add(data);
     location.href = `ticket.html?id=${doc.id}`;
 }

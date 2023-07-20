5cdadd7ef62c6f9ef0d32ac937af2599e88fff52
Jeff <jah5fv@virginia.edu>
2023-07-19 22:17:31 -0400

    Better printing

--- printer/printer.py
+++ printer/printer.py
@@ -10,9 +10,12 @@ app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 p = Usb(0x4b43, 0x3538, 0, 0x81, 0x03)
-def print_barcode(code):
-    p.barcode(code=code, function_type='B', bc='CODE128', height=128, width=2)
-    p.cut()
+def print_barcode(code, name):
+    p.text('\n\n\n\n')
+    p.barcode(code=code, function_type='B', bc='CODE128', height=128, width=2, pos="OFF")
+    p.text('\nID: ')
+    p.text(name)
+    p.cut(mode='PART')
 
 initializing = True
 def on_snapshot(snapshot, changes, read_time):
@@ -23,7 +26,7 @@ def on_snapshot(snapshot, changes, read_time):
     for change in changes:
         if change.type.name == "ADDED":
             print(f'Found new doc, printing barcode for {change.document.id}')
-            print_barcode(change.document.id)
+            print_barcode(change.document.id, change.document.to_dict()['name'])
 
 query = db.collection("tickets")
 query_watch = query.on_snapshot(on_snapshot)
